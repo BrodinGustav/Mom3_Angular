636299ac1cadf744067c84d7f1d57c178f07490d
Gustav Brodin <brodin.gustav@gmail.com>
2024-04-22 12:57:33 +0200

    Commit 5: Lagt in funktion för konvertering

--- src/app/convert/convert.component.ts
+++ src/app/convert/convert.component.ts
@@ -13,8 +13,22 @@ export class ConvertComponent {
 //Properties
  meter: number = 0;
  celcius: number = 0;
- result: number = 0;
+ resultFot: number = 0;
+ resultFahrenheit: number = 0;
 
  //Methods
+convertMeterToFoot(): void {
+this.resultFot = this.meter * 3,28084;  
+}
 
+convertCelciusToFahrenheit(): void{
+   this.resultFahrenheit = (this.celcius * 9/5) + 32;
+}
+
+clearConvert(): void{
+  this.meter = 0,
+  this.celcius = 0,
+  this.resultFot = 0;
+  this.resultFahrenheit = 0;
+}
 }
