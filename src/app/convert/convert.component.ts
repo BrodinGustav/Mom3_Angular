7e22361024a8d9d3e41377601dc550f48c82080b
Gustav Brodin <brodin.gustav@gmail.com>
2024-04-22 12:42:32 +0200

    Importert FormsModule samt påbörjat konverteringssida

--- src/app/convert/convert.component.ts
+++ src/app/convert/convert.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-convert',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './convert.component.html',
   styleUrl: './convert.component.css'
 })
 export class ConvertComponent {
+
+//Properties
+ meter: number = 0;
+ celcius: number = 0;
+ result: number = 0;
+
+ //Methods
 
 }
