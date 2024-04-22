fc524b25671bd4d7c76a53d296172e4462a1df15
Gustav Brodin <brodin.gustav@gmail.com>
2024-04-22 09:39:48 +0200

    Commit 2: Ordnat med routs för 3 st undersidor

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { IndexComponent } from './index/index.component';
+import { AboutComponent } from './about/about.component';
+import { ConvertComponent } from './convert/convert.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", component: IndexComponent},
+    { path: "about", component: AboutComponent},
+    {path: "convert", component: ConvertComponent}
+];
